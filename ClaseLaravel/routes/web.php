6beaa1b75d2d0513156e5fcea33b4d1aa1cad71f
Alberth-h <jesalbertohyfx@hotmail.com>
2021-01-26 10:11:34 -0700

    rutas

--- ClaseLaravel/routes/web.php
+++ ClaseLaravel/routes/web.php
@@ -11,8 +11,39 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
 
+*/
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+/*
+Route::get('/', function () {
+    return 'aa sos tu mi oana';
+});
+*/
+
+/*
+Route::get('/', function () {
+    return [
+        'Clases' =>[
+            'Instalacion de laravel',
+            'Rutas Basicas de laravel',
+            'Controladores de laravel',
+            ]
+        ];
+});
+*/
+Route::get('clases', function () {
+    return 'Aqui ese listarian todas las clases';
+});
+
+Route::get('clases/crear', function () {
+    return 'Aqui se crean todas las clases';
+});
+
+Route::get('clases/{id}/editar', function ($id) {
+    return 'Aqui se edita la clase con id: ' .$id;
 });
